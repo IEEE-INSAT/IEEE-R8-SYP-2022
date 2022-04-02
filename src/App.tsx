7945f7cdb19c2d8410b86e55dfe8050bd0d9385e
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-04-02 21:10:59 +0100

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Home.css';
+
+export default function Home(){
+    return (
+        <div>
+            <h1>Welcome !</h1>
+        </div>
+    )
+}
