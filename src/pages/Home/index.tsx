d78b6a388941605c716e1e9204e3a33df8973468
Habib Sellami <habib.sellami@outlook.com>
2022-04-03 17:50:59 +0100

    add map with leaflet.js in Location component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import './Home.css';
-
+import L from 'leaflet';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 export default function Home() {
+
+    const pinIcon = L.icon({
+            iconUrl: require('../../assets/img/CapitalPin.png'),
+            iconSize: [20, 45]
+        }
+    )
     return (
 
         <div className={"Location"}>
             <h4>EVENT LOCATION</h4>
             <h2>Location</h2>
-            <div className={"map-responsive"}>
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3245554.5160673875!2d8.461073039596368!3d35.14763486835344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fd337f5e7ef543%3A0xd671924e714a0275!2sTunis!5e0!3m2!1sen!2stn!4v1648990623167!5m2!1sen!2stn"
-                    width="600" height="450" style={{border: 0}} allowFullScreen={false} loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"/>
+            <div id="map">
+                <MapContainer center={[36.8001, 10.1864]} zoom={7}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
+                    />
+                    <Marker position={[36.8001, 10.1864]} icon={pinIcon}>
+                        <Popup>
+                            Add info about Tunis here <br/> Remove if unnecessary
+                        </Popup>
+                    </Marker>
+                    <Marker position={[36.4047, 10.6191]}>
+                        <Popup>
+                            Add info about Tunis here <br/> Remove if unnecessary
+                        </Popup>
+                    </Marker>
+                </MapContainer>
             </div>
         </div>
     )
