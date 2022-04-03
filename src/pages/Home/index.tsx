dac641dada0541bd1afe194af08ba4e0b46ab6c5
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-04-03 17:45:55 +0100

    create the page Header

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Header from "../../components/Header";
 import './Home.css';
 
 export default function Home(){
     return (
         <div>
+            <Header />
             <h1>Welcome !</h1>
         </div>
     )
