590a45e80b471b9152d3b70cff06b98f4431da86
anascherni <cherniii.anas@gmail.com>
2022-04-05 02:28:26 +0100

    isolated Card component from CardSlider

---
+++ src/carousel/Card.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Facebook from "./assets/FB.png"
+import linkedin from "./assets/LinkedIN.png"
+import whatsapp from "./assets/wapp.png"
+import line from "./assets/Line1.png"
+import './Card.css'
+import PropTypes from 'prop-types';
+
+
+
+const Card=(props) => {
+    
+    return(
+        <div className="slider-card" >
+                            
+                            <div className="slider-card-image" style={{backgroundImage:`url(${props.imgURL})`}}>
+                                <div className="slider-card-info">
+                                    <section className="slider-card-social-media-section">
+                                        <a href=""><img alt="facebook" className="slider-card-social-media" src={Facebook}></img></a>
+                                        <a href=""><img alt="linkedin" className="slider-card-social-media" src={linkedin}></img></a>
+                                        <a href=""><img alt="whatsapp" className="slider-card-social-media" src={whatsapp}></img></a>
+                                    </section>
+                                    <p className="slider-card-name">{props.name}</p>
+                                    <section className="slider-card-line">
+                                        <img alt="line" src={line}></img>
+                                    </section>
+                                    <p className="slider-card-position">{props.position}</p>
+                                    
+                                </div>
+                            </div>  
+                        </div>
+
+
+
+    )
+} 
+Card.PropTypes = {
+    imgURL: PropTypes.any,
+    name: PropTypes.any,
+    position: PropTypes.any
+};
+
+export default Card;
--- src/carousel/CardSlider.jsx
+++ src/carousel/CardSlider.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import './Slider.css'
 import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill} from 'react-icons/bs'
-import Facebook from "./assets/FB.png"
-import linkedin from "./assets/LinkedIN.png"
-import whatsapp from "./assets/wapp.png"
-import line from "./assets/Line1.png"
+import Card from './Card'
 
 const CardSlider =(props) =>{
     const slides = [{name:'Khaldoun Taktak',position:'Congress Chairman',imageURL:'Khaldoun 1.png'},
@@ -30,24 +27,7 @@ const CardSlider =(props) =>{
             {
                 slides.map((slide,index) =>{
                     return(
-                        <div className="slider-card" key={index}>
-                            
-                            <div className="slider-card-image" style={{backgroundImage:`url(${slide.imageURL})`}}>
-                                <div className="slider-card-info">
-                                    <section className="slider-card-social-media-section">
-                                        <a href=""><img alt="facebook" className="slider-card-social-media" src={Facebook}></img></a>
-                                        <a href=""><img alt="linkedin" className="slider-card-social-media" src={linkedin}></img></a>
-                                        <a href=""><img alt="whatsapp" className="slider-card-social-media" src={whatsapp}></img></a>
-                                    </section>
-                                    <p className="slider-card-name">{slide.name}</p>
-                                    <section className="slider-card-line">
-                                        <img alt="line" src={line}></img>
-                                    </section>
-                                    <p className="slider-card-position">{slide.position}</p>
-                                    
-                                </div>
-                            </div>  
-                        </div>
+                        <Card index={index} imgURL={slide.imageURL} name={slide.name} position={slide.position} key={index}/>
                     )
 
                 })
