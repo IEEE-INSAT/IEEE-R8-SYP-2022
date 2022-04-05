9261db53b7f896b2a982c20b452fc59c41849224
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-04-05 20:56:26 +0100

    Fix Merge Errors

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,7 @@ import AboutLocation from "../../components/AboutLocation";
 
 import Footer from "../../components/Footer";
 import Header from "../../components/Header" ;
+import Location from "../../components/Location";
 import Sponsors from "../../components/Sponsors";
 import Testimonials from "../../components/Testimonials";
 import sponsorsList from "../../data/sponsors.json";
@@ -13,13 +14,14 @@ import './Home.css';
 
 export default function Home() {
     return (
-        <div>
+        <div className="home">
             <Header />
             <AboutEvent />
+            <Location />
             <AboutLocation />
-            <Sponsors items={sponsorsList["sponsors"]}/>
             <Testimonials />
-
+            <Sponsors items={sponsorsList["sponsors"]}/>
+            <Footer />
         </div>
     )
 }
