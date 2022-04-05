2410bfe1759cad9db6e2241844aba1d7bb8a9c73
Habib Sellami <habib.sellami@outlook.com>
2022-04-05 18:19:45 +0100

    finished web testimonial component

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,29 +1,77 @@
 import React from "react";
+import {useState, useEffect} from "react";
+
 import data from "./testimonials-data";
-import orangeCircle from"./assets/OrangeCircle.png";
-import blueCircle from"./assets/BlueCircle.png";
+import orangeCircle from "./assets/OrangeCircle.png";
+import blueCircle from "./assets/BlueCircle.png";
 
 import './Testimonials.css';
 
 export default function Testimonials() {
-    const test = data;
+    const [people] = useState(data);
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const lastIndex = people.length - 1;
+        if (index < 0) {
+            setIndex(lastIndex);
+        }
+        if (index > lastIndex) {
+            setIndex(0);
+        }
+    }, [index, people]);
+
+    useEffect(() => {
+        const slider = setInterval(() => {
+            setIndex(index + 1);
+        }, 5000);
+        return () => {
+            clearInterval(slider);
+        };
+    }, [index]);
+
+    interface person {
+        id: number;
+        name: string;
+        role: string;
+        description: string;
+        image: any;
+    }
+
     return (
         <div className="testimonials-container">
             <h4>WHAT OTHERS SAY</h4>
             <h2>Testimonials</h2>
-
-            <div className="testimonial">
-
-                <div className="testimonial-text-container">
-                    <p className="description">{test[0].description}</p>
-                    <span className="name">{test[0].name}</span>
-                    <span className="role">{test[0].role}</span>
-                </div>
-                <div className="testimonial-image-container">
-                    <img className="main-image" src={test[0].image} alt={test[0].name}/>
-                    <img className="orange-circle" src={orangeCircle} alt=""/>
-                    <img className="blue-circle" src={blueCircle} alt=""/>
-
+            <div className="section-center">
+                {
+                    people.map((item: person, indexPeople) => {
+                        const {id, name, role, description, image} = item;
+                        let position = "nextSlide";
+                        if (indexPeople === index) {
+                            position = "activeSlide";
+                        }
+                        if (indexPeople === index - 1 || (index === 0 && indexPeople === people.length - 1)) {
+                            position = "lastSlide";
+                        }
+                        return (
+                            <div className={"testimonial " + position} key={id}>
+                                <div className="testimonial-text-container">
+                                    <p className="description">{description}</p>
+                                    <span className="name">{name}</span>
+                                    <span className="role">{role}</span>
+                                </div>
+                                <div className="testimonial-image-container">
+                                    <img className="main-image " src={image} alt={name}/>
+                                    <img className="orange-circle" src={orangeCircle} alt=""/>
+                                    <img className="blue-circle" src={blueCircle} alt=""/>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+                <div className="buttons">
+                    <button className="prev" onClick={() => setIndex(index - 1)}/>
+                    <button className="next" onClick={() => setIndex(index + 1)}/>
                 </div>
             </div>
         </div>
