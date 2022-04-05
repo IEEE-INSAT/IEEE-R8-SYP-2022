85e660d7cc9d979a26a94b7c2df83f8864bbb055
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-04-05 20:03:30 +0100

    Fix merge conficts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutEvent from "../../components/AboutEvent";
+import Footer from "../../components/Footer";
 import Header from "../../components/Header" ;
 import Sponsors from "../../components/Sponsors";
 import Testimonials from "../../components/Testimonials";
@@ -10,7 +11,10 @@ import './Home.css';
 export default function Home() {
     return (
         <div>
+            <Header />
+            <AboutEvent />
             <Sponsors items={sponsorsList["sponsors"]}/>
+            <Testimonials />
         </div>
     )
 }
