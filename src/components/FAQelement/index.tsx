1476b628a39ecd97bbd0305b0ba102d8600f7dc5
Habib Sellami <habib.sellami@outlook.com>
2022-04-13 22:36:28 +0100

    add initial faq page

---
+++ src/components/FAQelement/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import './faq.css';
+
+interface question {
+    category: string,
+    question: string,
+    answer: string,
+    open: boolean,
+}
+
+interface FAQProps {
+    faq: question;
+    index: number;
+    toggleFAQ: (index: number,category : string) => void;
+    key?: number;
+}
+
+export default function FAQelement({faq,index,toggleFAQ}:FAQProps) : JSX.Element {
+
+    return (
+        <div
+            className={"faq " + (faq.open ? 'open' : '')}
+            key={index}
+            onClick={() => toggleFAQ(index,faq.category)}
+        >
+            <div className="faq-question">
+                {faq.question}
+            </div>
+            <div className="faq-answer">
+                {faq.answer}
+            </div>
+        </div>
+    )
+}
