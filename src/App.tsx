f3b33c4206f03cf48c3c0be2df1d8dc53a630956
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-04-19 17:35:44 +0100

    change routing

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,19 @@ import Home from './pages/Home';
 import FAQ from './pages/FAQ';
 import AboutTn from './pages/AboutTn';
 import ApplyForFunding from './pages/ApplyForFunding';
-import UnderConstruction from './pages/UnderConstruction';
 
 
 function App() {
   return (
     <div className="App">
-      <UnderConstruction />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/tunisia" element={<AboutTn />} />
+          <Route path="/applyforfunding" element={<ApplyForFunding />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
