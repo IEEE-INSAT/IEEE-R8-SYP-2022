8362d48677ce950848e995ee5144a999dbddd831
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-05-19 16:54:40 +0100

    Schedule form deployement

--- src/pages/ApplyForFunding/index.tsx
+++ src/pages/ApplyForFunding/index.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import Fade from "react-reveal/Fade";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
@@ -11,9 +7,8 @@ import Header from "../../components/Header";
 export default function ApplyForFunding(){
     return (
         <div className="applyForFunding">
-            <Fade>
-                <Header title={"APPLY FOR FUNDING"} description="The form will be available soon... "/>
-            </Fade>  
+            <Header title={"APPLY FOR FUNDING"} />
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfz6Pmg4dJeODT3fyeKYLSLaA3rmELCYvX1PsSWrwajQ2vBvQ/viewform?embedded=true" width="100%" height="1387" frameBorder="0" marginHeight={0} marginWidth={0}>Loading…</iframe>
             <Footer/>
         </div>
     )
