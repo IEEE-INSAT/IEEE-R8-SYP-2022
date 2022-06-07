c4290ad0cd7739c46af0c9ac78cec874435297d8
M0ngi <saidanemongi@gmail.com>
2022-06-07 19:24:28 +0100

    Show components in Schedule page

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import MeetYourDestination from "../../components/MeetYourDestination";
 import MiddleSection from "../../components/MiddleSection";
 
 export default function Schedule() {
@@ -8,6 +10,10 @@ export default function Schedule() {
             <Header title={"SCHEDULE"} description={<p>The congress expected schedule</p>} />
 
             <MiddleSection />
+
+            <MeetYourDestination />
+
+            <Footer />
         </>
     );
 }
