f749fd7d436b3e7b82ba5841077376b49c4bc4cb
SellamiHabib <habib.sellami@outlook.com>
2022-06-15 21:20:13 +0100

    small typography tweak

--- src/pages/Speakers/index.tsx
+++ src/pages/Speakers/index.tsx
@@ -10,7 +10,7 @@ import Fade from "react-reveal/Fade";
 export default function SpeakersPage() {
     return (
         <div className="registration">
-            <Header title={'SPEAKERS'} description={<p>MEET OUR SPEAKERS</p>}/>
+            <Header title={'SPEAKERS'} description={<p>Meet our speakers</p>}/>
             <Fade>
              <Speakers/>
             </Fade>
