b4fb02dd0fa81d729ab0e5df8bd710a3b0626ab8
SellamiHabib <habib.sellami@outlook.com>
2022-06-21 13:01:32 +0100

    added on hover effects for sponsors component

--- src/components/Sponsors/index.tsx
+++ src/components/Sponsors/index.tsx
@@ -3,12 +3,11 @@ import './Sponsors.css';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import Fade from "react-reveal/Fade";
-import ThemedButton from '../ThemedButton';
-import { Link } from 'react-router-dom';
 
 interface SponsorItem {
     name: string;
     filename: string;
+    type: string
 }
 
 interface SponsorsProps {
@@ -18,15 +17,21 @@ interface SponsorsProps {
 
 export default function Sponsors({items}: SponsorsProps) {
     return (
-        <div className="sponsors-container">
-            <div className="sponsor-logo-container">
-                {
-                    items.map((sponsor, i) => (
-                        <Fade key={i}>
-                            <img className="sponsor-logos" src={require(`../../assets/images/sponsors/${sponsor.filename}`)} alt={sponsor.name} />
-                        </Fade>
-                    ))
-                } </div>
-  </div>
+        <div className="sponsor-logo-row" >
+            {
+
+                items.map((sponsor, i) => (
+                    <Fade key={i}>
+                        <div className="sponsor-container">
+                            <img className="sponsor-logos"
+                                 src={require(`../../assets/images/sponsors/${sponsor.filename}`)} alt={sponsor.name}/>
+                           <div className="sponsor-type-overlay">
+                               <span>{sponsor.type}</span>
+                           </div>
+                        </div>
+
+                    </Fade>
+                ))
+            } </div>
     );
 }
