0210b33f4d9049fb8c1b5ca78f1e6b5acb147502
ahmedbechirmezhoud <79605129+ahmedbechirmezhoud@users.noreply.github.com>
2022-06-28 12:14:01 +0100

    add Register Now Button

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -23,12 +23,13 @@ export default function NavBar({ navItems }: NavBarProps){
         <div className={active ? "MenuIcon active" : "MenuIcon"} onClick={() => setActive(!active)}>
             <div className="bar1"></div>
             <div className="bar2"></div>
-            <div className="bar3"></div>
+            <div className="bar3"></div> 
         </div>
         <div className={active ? "items active" : "items"} >
             {navItems.map((item, i) => (
                 <Link to={item.path} key={i} className="NavItem">{item.name}</Link>
             ) )}
+            <a target='_blank' rel="noreferrer" href="https://forms.gle/SiZqTqL5J978S1uk6" ><ThemedButton style={{ margin : "10px 10px 0 0" }} text="Register Now" color="secondary" mode="dark" /></a>
         </div>
     </nav>
     )
