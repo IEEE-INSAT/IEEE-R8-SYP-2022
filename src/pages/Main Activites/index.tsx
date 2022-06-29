3149b8427a596b7b2e9244dadc703fee13521e3a
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2022-06-29 21:59:09 +0100

    changed title

--- src/pages/Main Activites/index.tsx
+++ src/pages/Main Activites/index.tsx
@@ -7,6 +7,10 @@ import Header from '../../components/Header';
 import Fade from "react-reveal/Fade";
 export default function MainActivities(){
     return(
-        <div></div>
+        <div className="registration">
+            <Header title={'Main Activities'} description={<p>See what&#39;s in the menu</p>}/>
+            <div style={{marginBottom:100}}></div>
+            <Footer/>
+        </div>
     );
 }
