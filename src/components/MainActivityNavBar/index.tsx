1860d2f7eb126282d607a68b795649cd63a0a961
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2022-06-30 14:25:04 +0100

    finished styling mobile version of navbar

--- src/components/MainActivityNavBar/index.tsx
+++ src/components/MainActivityNavBar/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './style.css'
 
 interface MainActivityNavBarProps{
@@ -7,10 +7,20 @@ interface MainActivityNavBarProps{
     default : string
 }
 export default function MainActivityNavBar(props : MainActivityNavBarProps){
+    const [selected,setSelected]=useState(props.items[1]);
     const list=props.items.map((item,index)=>
-    <div className="MainActivityNavBarItems"  key={index}>
-        
-        <input type="radio" id={`${index}`} name="NavBarItems" value={`${item}`} defaultChecked={props.default===item} onChange={(event)=>props.changeView(event.target.value)}/>
+    <div className={`MainActivityNavBarItems ${selected===item?"selectedItem":""}`}  key={index}>
+        <input 
+            type="radio" 
+            id={`${index}`} 
+            name="NavBarItems" 
+            value={`${item}`} 
+            defaultChecked={props.default===item} 
+            onChange={(event)=>{
+                props.changeView(event.target.value);
+                setSelected(event.target.value);
+            }}
+        />
         <label htmlFor={`${index}`} >{item}</label>
     </div>
     )
