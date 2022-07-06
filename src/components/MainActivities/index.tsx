4c36e070cc3ae57ef413287101430aa9e05cea62
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2022-07-06 20:11:33 +0100

    FINISHED

--- src/components/MainActivities/index.tsx
+++ src/components/MainActivities/index.tsx
@@ -1,57 +1,120 @@
-import MainActivityNavBar from "../MainActivityNavBar";
-import ActivityPage from "../ActivityPage";
-import ActivityList from "../ActivityList";
-import activities from '../../data/MainActivities.json'
-import React,{ useState } from "react";
-import './style.css'
-export default function MainActivities(){
-    
+import MainActivityNavBar from '../MainActivityNavBar';
+import ActivityPage from '../ActivityPage';
+import ActivityList from '../ActivityList';
+import activities from '../../data/MainActivities.json';
+import React, { useState } from 'react';
+import './style.css';
+export default function MainActivities() {
     //state/view management
-    const [type,setType]=useState(activities[0].type);
-    const [activity,setActivity]=useState(activities[0].id)
-    function changeType(a:string){
+    const [type, setType] = useState(activities[0].type);
+    const [activity, setActivity] = useState(activities[0].id);
+    function changeType(a: string) {
         setType(a);
-        setActivity(mainActivities[(a as ('Workshop'|'Plenary'|'Keynote'))][0].id);
+        setActivity(mainActivities[a as 'Workshop' | 'Plenary' | 'Keynote'][0].id);
     }
-    function changeActivity(id:number){
+    function changeActivity(id: number) {
         setActivity(id);
+        setViewPage(true);
     }
-    const empty={
+    const empty = {
         id: 0,
-        type: "",
-        name: "",
-        description: "",
-        date: "",
-        time: "",
-        location: "",
-        instructorName: "",
-        instructorDescription: "",
-        instructorHighlight: "",
-        linkedin: ""
+        type: '',
+        name: '',
+        description: '',
+        date: '',
+        time: '',
+        location: '',
+        instructorName: '',
+        instructorDescription: '',
+        instructorHighlight: '',
+        linkedin: '',
     };
 
-
-    const Workshop: { id:number, type: string; name: string; description: string; date: string; time: string; location: string; instructorName: string; instructorDescription: string; instructorHighlight: string; linkedin: string; }[]=[];
-    const Keynote: { id:number, type: string; name: string; description: string; date: string; time: string; location: string; instructorName: string; instructorDescription: string; instructorHighlight: string; linkedin: string; }[]=[];
-    const Plenary: { id:number, type: string; name: string; description: string; date: string; time: string; location: string; instructorName: string; instructorDescription: string; instructorHighlight: string; linkedin: string; }[]=[];
-    activities.forEach((item)=>{
+    const Workshop: {
+        id: number;
+        type: string;
+        name: string;
+        description: string;
+        date: string;
+        time: string;
+        location: string;
+        instructorName: string;
+        instructorDescription: string;
+        instructorHighlight: string;
+        linkedin: string;
+    }[] = [];
+    const Keynote: {
+        id: number;
+        type: string;
+        name: string;
+        description: string;
+        date: string;
+        time: string;
+        location: string;
+        instructorName: string;
+        instructorDescription: string;
+        instructorHighlight: string;
+        linkedin: string;
+    }[] = [];
+    const Plenary: {
+        id: number;
+        type: string;
+        name: string;
+        description: string;
+        date: string;
+        time: string;
+        location: string;
+        instructorName: string;
+        instructorDescription: string;
+        instructorHighlight: string;
+        linkedin: string;
+    }[] = [];
+    activities.forEach((item) => {
         switch (item.type) {
-            case "Workshop": {Workshop.push(item); break;}
-            case "Keynote": {Keynote.push(item); break;}
-            case "Plenary": {Plenary.push(item); break;}
+            case 'Workshop': {
+                Workshop.push(item);
+                break;
+            }
+            case 'Keynote': {
+                Keynote.push(item);
+                break;
+            }
+            case 'Plenary': {
+                Plenary.push(item);
+                break;
+            }
         }
     });
-    const mainActivities={Workshop,Keynote,Plenary}
-    return(
+    const mainActivities = { Workshop, Keynote, Plenary };
+
+    const [viewPage, setViewPage] = useState(false);
+    return (
         <div className="MainActivities">
-                <MainActivityNavBar 
-                    items={['Keynote','Workshop','Plenary']} 
-                    changeView={changeType } default={activities[0].type}/>
-            
+            <MainActivityNavBar
+                items={['Keynote', 'Workshop', 'Plenary']}
+                changeView={changeType}
+                default={activities[0].type}
+            />
+
             <div className="mainContainer">
-                <div className="listContainer"><ActivityList activities={mainActivities[(type as ('Workshop'|'Plenary'|'Keynote'))]} 
-                changeView={changeActivity}/></div>
-                <div className="pageContainer"><ActivityPage activity={mainActivities[(type as ('Workshop'|'Plenary'|'Keynote'))].find(element=>element.id==activity) || empty} /></div>
+                <div className={`${viewPage ? 'hidden ' : 'showed '}listContainer`}>
+                    <ActivityList
+                        activities={mainActivities[type as 'Workshop' | 'Plenary' | 'Keynote']}
+                        changeView={changeActivity}
+                    />
+                </div>
+                <div className={`pageContainer${viewPage ? ' showed' : ' hidden'}`}>
+                    <div className="backButton" onClick={() => setViewPage(false)}>
+                        <i className="fa-solid fa-backward"></i> Back
+                    </div>
+                    <ActivityPage
+                        activity={
+                            mainActivities[type as 'Workshop' | 'Plenary' | 'Keynote'].find(
+                                (element) => element.id == activity,
+                            ) || empty
+                        }
+                    />
+                </div>
             </div>
         </div>
     );
