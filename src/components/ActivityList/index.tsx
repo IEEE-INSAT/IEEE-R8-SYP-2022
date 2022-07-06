c6db6e526b4a221942628206021c7623c4e517ff
mahdichaari01 <92325467+mahdichaari01@users.noreply.github.com>
2022-07-06 13:03:30 +0100

    improved selection logic in activity list

--- src/components/ActivityList/index.tsx
+++ src/components/ActivityList/index.tsx
@@ -2,14 +2,21 @@ import React, { useState } from 'react';
 import './style.css';
 
 interface ActivityListProps {
-    activities: { id: number, name: string; instructorName: string }[];
+    activities: { id: number; name: string; instructorName: string }[];
     changeView(a: number): any;
 }
 
 export default function ActivityList(props: ActivityListProps) {
     const [selected, setSelected] = useState(props.activities[0].id);
     const list = props.activities.map((item, index) => (
-        <div className={`ActivityListItem ${selected === item.id ? 'selectedActivityListItem' : ''}`} key={index}>
+        <div
+            className={`ActivityListItem ${selected === item.id ? 'selectedActivityListItem' : ''}`}
+            key={index}
+            onClick={()=>{
+                props.changeView(item.id);
+                setSelected(item.id)
+            }}
+        >
             <input
                 type="radio"
                 id={`${item.name}`}
@@ -20,6 +27,7 @@ export default function ActivityList(props: ActivityListProps) {
                     props.changeView(parseInt(event.target.value));
                     setSelected(parseInt(event.target.value));
                 }}
+                checked={item.id === selected}
             />
             <label htmlFor={`${item.name}`}>
                 <b>{item.name}</b>
@@ -27,8 +35,6 @@ export default function ActivityList(props: ActivityListProps) {
             </label>
         </div>
     ));
-
-    
 
     return <div className="ActivityList">{list}</div>;
 }
