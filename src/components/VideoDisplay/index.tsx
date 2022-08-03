d930b07ce8331fe1856baed5979014813c1b3fed
SellamiHabib <habib.sellami@outlook.com>
2022-08-03 12:49:22 +0100

    add sponsor

--- src/components/VideoDisplay/index.tsx
+++ src/components/VideoDisplay/index.tsx
@@ -8,12 +8,16 @@ interface VideoDisplayProps {
 }
 
 export default function VideoDisplay({link}: VideoDisplayProps) {
+    let isLocal = true;
+    isLocal = !(link.split(':')[0] === 'https');
+    console.log(link, link.split(':')[0], isLocal);
     return (
         <div className='videoContainer blockElement'>
             <img className="deco top-disk" src={top_disk}/>
             <img className="deco bottom-disk" src={bot_disk}/>
 
             <div className="video">
+                {}
                 <iframe className={"videoImage"} width="1280" height="720"
                         src={link} title="True Tunisia" frameBorder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
