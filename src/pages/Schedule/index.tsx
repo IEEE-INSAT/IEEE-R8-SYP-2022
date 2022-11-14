95c8ba500b5eb8d0412617bb40e115dad5b2bab9
Mahdi Chaari <mahdi.chaari2@insat.u-carthage.tn>
2022-11-14 09:01:50 +0100

    added link to schedule page (#30)

--- src/pages/Schedule/index.tsx
+++ src/pages/Schedule/index.tsx
@@ -1,36 +1,44 @@
-import React, {createRef, useRef} from "react";
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
-import MiddleSection from "../../components/MiddleSection";
-import ScheduleSection from "../../components/ScheduleSection";
-import Gallery from "../../components/Gallery";
+import React, { createRef, useRef } from 'react';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import MiddleSection from '../../components/MiddleSection';
+import ScheduleSection from '../../components/ScheduleSection';
+import Gallery from '../../components/Gallery';
 
 import ScheduleDescription from '../../data/program/ScheduleDescription.json';
-import ScrollToTop from "../../components/ScrollToTop";
-
+import ScrollToTop from '../../components/ScrollToTop';
 
 export interface IScheduleDescription {
-    scrollRefs: React.MutableRefObject<React.RefObject<HTMLDivElement>[]>
+    scrollRefs: React.MutableRefObject<React.RefObject<HTMLDivElement>[]>;
 }
 
 export default function Schedule() {
     const scrollRefs = useRef([]);
 
     // Populate scrollable refs, only create them once
-    scrollRefs.current = [...Array(ScheduleDescription.length)].map(
-        (_, i) => scrollRefs.current[i] ?? createRef()
-    );
+    scrollRefs.current = [...Array(ScheduleDescription.length)].map((_, i) => scrollRefs.current[i] ?? createRef());
     return (
         <>
-            <Header title={"SCHEDULE"} description={<p>The congress&#39; expected schedule</p>}/>
-            <ScheduleSection scrollRefs={scrollRefs}/>
-            <MiddleSection scrollRefs={scrollRefs}/>
-            <Gallery
-                title="Meet your destination"
-                description="An Album of photos of your destination"
+            <Header
+                title={'SCHEDULE'}
+                description={
+                    <p>
+                        Find out more in a{' '}
+                        <a
+                            style={{ color: '#0042a5' }}
+                            href="https://drive.google.com/file/d/1ekD-eG9PNZbE1VwE__z1rP1vzBP73Xry/view"
+                        >
+                            more detailed version
+                        </a>{' '}
+                        of the shcedule
+                    </p>
+                }
             />
-            <ScrollToTop/>
-            <Footer/>
+            <ScheduleSection scrollRefs={scrollRefs} />
+            <MiddleSection scrollRefs={scrollRefs} />
+            <Gallery title="Meet your destination" description="An Album of photos of your destination" />
+            <ScrollToTop />
+            <Footer />
         </>
     );
 }
