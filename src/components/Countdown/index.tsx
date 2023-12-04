fbba33fd7e3daf4f64a7aa07154d5fe17fb94a0a
Habib Sellami <habib.sellami@outlook.com>
2023-12-04 11:56:21 +0100

    Add aftermovie

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Countdown from 'react-countdown';
 import "./Countdown.css"
 
-interface rendererprops{
+interface rendererprops {
     days: number;
     hours: number;
     minutes: number;
@@ -10,48 +10,55 @@ interface rendererprops{
     completed: boolean;
 }
 
-export default function CountdownSection(){
+export default function CountdownSection() {
 
-    const renderer = ({ days, hours, minutes, seconds, completed } : rendererprops ) => {
+    const renderer = ({days, hours, minutes, seconds, completed}: rendererprops) => {
         if (completed) {
-          // Render a completed state
-          return (
-            <div className="countdown" >
-                <h4>EVENT TIMER</h4>
-                <h2>The event has ended and lifelong memories have been made! Highlights will soon be posted in our social media and in the website.</h2>
-            </div>
-          );
+            // Render a completed state
+            return (
+                <div className="countdown">
+                    <h4>EVENT TIMER</h4>
+                    <iframe
+                        src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2FR8SYP%2Fvideos%2F5106424512800674%2F&show_text=false&width=560&t=0"
+                        width="560" height="314"
+                        style={{border: "none", overflow: "hidden"}}
+                        scrolling="no" frameBorder="0"
+                        allowFullScreen
+                        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                    ></iframe>
+                </div>
+            );
         } else {
-          // Render a countdown
-          return ( 
-        <div className="countdown" >
-          <h4>EVENT TIMER</h4>
-          <h2>COMING SOON IN</h2>
-          <div className="timer" >
-              <div>
-                  <p className="numbers">{days}</p>
-                  <p className="unit">days</p>
-              </div>
-              <div>
-                  <p className="numbers" >{hours}</p>
-                  <p className="unit" >hours</p>
-              </div>
-              <p className="numbers">:</p>
-              <div>
-                  <p className="numbers" >{minutes}</p>
-                  <p className="unit" >minutes</p>
-              </div>
-              <p className="numbers">:</p>
-              <div>
-                  <p className="numbers">{seconds}</p>
-                  <p className="unit" >seconds</p>
-              </div>
-          </div>
-      </div>)
+            // Render a countdown
+            return (
+                <div className="countdown">
+                    <h4>EVENT TIMER</h4>
+                    <h2>COMING SOON IN</h2>
+                    <div className="timer">
+                        <div>
+                            <p className="numbers">{days}</p>
+                            <p className="unit">days</p>
+                        </div>
+                        <div>
+                            <p className="numbers">{hours}</p>
+                            <p className="unit">hours</p>
+                        </div>
+                        <p className="numbers">:</p>
+                        <div>
+                            <p className="numbers">{minutes}</p>
+                            <p className="unit">minutes</p>
+                        </div>
+                        <p className="numbers">:</p>
+                        <div>
+                            <p className="numbers">{seconds}</p>
+                            <p className="unit">seconds</p>
+                        </div>
+                    </div>
+                </div>)
         }
     };
 
-    return(
+    return (
         <Countdown
             date={new Date("3 Aug 2022")}
             renderer={renderer}
